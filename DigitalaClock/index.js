87d44cccd3aac10c8a4b343bb03d2c084fd8559b
muktiswarup <csjena1999@gmail.com>
2025-02-17 17:16:18 +0530

    l

--- DigitalaClock/index.js
+++ DigitalaClock/index.js
@@ -1,15 +1,20 @@
-let hour=document.querySelector(".hour");
-let minute=document.querySelector(".minute");
-let second=document.querySelector(".Second");
+const hour = document.querySelector(".hour")
+const minute = document.querySelector(".minute")
+const second= document.querySelector(".second")
+const ampm = document.querySelector(".ampm");
+setInterval(()=>{
+    const date = new Date();
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const ampmm = hours >= 12 ? 'PM' : 'AM';
 
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
 
-function Digital(){
-    let date = new Date();
-    hour.innerText=date.getHours();
-    minute.innerText=date.getMinutes();
-    second.innerText=date.getSeconds();
-    
-    requestAnimationFrame(Digital)
-}
-requestAnimationFrame(Digital)
-Digital();
+    hour.innerHTML = (hours < 10 ? "0" : "") + hours;
+    minute.innerHTML = (minutes < 10 ? "0" : "") + minutes;
+    second.innerHTML = (seconds < 10 ? "0" : "") + seconds;
+    ampm.innerHTML = ampmm; // Update the period element with AM/PM
+},1000)
+
